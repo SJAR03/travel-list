17b35760fefb12efe3a8d34860d0b19e95cf7745
SJAR03 <sayerdisrodriguez@gmail.com>
2023-09-26 17:03:23 -0600

    First style and components structure

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Logo.css'
+
+const Logo = () => {
+  return (
+    <div className='background'>
+      <h1 className='title'>Lista de viaje</h1>
+    </div>
+  )
+}
+
+export default Logo
